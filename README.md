b2b38fc5b141adfc0062f574b712d69b9ae2a403
Zach Carrington <zdc@illinois.edu>
2024-03-12 15:05:37 -0500

    Initial development

---
+++ README.md
@@ -0,0 +1,13 @@
+# About
+
+A simple client for Aruba Clearpass that allows enabling, disabling, and querying mac addresses.
+
+## Support
+
+This product is supported by the Cybersecurity Development Team at the University of Illinois on a best-effort basis.
+
+As of the last update to this README, the expected End-of-Life and End-of-Support dates of this product are October 2025.
+
+End-of-Life was decided upon based on these dependencies:
+
+    - Python 3.9 (31 October 2025)
---
+++ src/clearpass/client.py
@@ -0,0 +1,165 @@
+import logging
+import re
+import requests
+import json
+import urllib.parse
+
+# ** Documentation **
+# https://cplab.techservices.illinois.edu/api-docs
+
+logger = logging.getLogger(__name__)
+
+macaddress1 = re.compile(
+    r'''^[0-9a-f]{12}$''')                     # e.g., A1B2C3D4E5F6
+macaddress2 = re.compile(
+    r'''^(?:[0-9a-f]{2}:){5}[0-9a-f]{2}$''')   # e.g., A1:B2:C3:D4:E5:F6
+macaddress3 = re.compile(
+    r'''^(?:[0-9a-f]{4}\.){2}[0-9a-f]{4}$''')  # e.g., A1B2.C3D4.E5F6
+macaddress4 = re.compile(
+    r'''^(?:[0-9a-f]{2}\-){5}[0-9a-f]{2}$''')  # e.g., A1-B2-C3-D4-E5-F6
+
+
+# Make sure that any mac address is properly formatted
+def normalize_mac_address(macstring):
+    m = macstring.lower()
+    if macaddress1.match(m):
+        return m
+    if macaddress2.match(m):
+        return re.sub(r''':''', '', m)
+    if macaddress3.match(m):
+        return re.sub(r'''\.''', '', m)
+    if macaddress4.match(m):
+        return re.sub(r'''\-''', '', m)
+    return None
+
+
+# Convert an all lowercase mac to a hyphenated string of macs
+def hyphenate_mac(macstring):
+    m = normalize_mac_address(macstring)
+    if not m:
+        logger.warning(f"Hyphenating {macstring} failed")
+        return macstring
+    evenchar = True
+    retmac = ""
+    for char in m:
+        evenchar = not evenchar
+        retmac += char
+        if evenchar:
+            retmac += "-"
+    return retmac.upper()[:-1]
+
+
+class APIConnection():
+    def __init__(self, username, password, endpoint, clientid, secret):
+        self._baseurl = f"https://{endpoint}/"
+
+        self._authurl = f"{self._baseurl}api/oauth"
+        self._authpayload = {
+            "grant_type": "password",
+            "username": username,
+            "password": password,
+            "client_id": clientid,
+            "client_secret": secret,
+        }
+        self._postheaders = {'Content-Type': 'application/json'}
+
+        self._macinfo = {}
+
+        self._access = self._get_access_token()
+        self._getheaders = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {self._access}',
+         }
+        self._postheaders.update(self._getheaders)
+
+    def _get_access_token(self):
+        res = requests.post(
+            url=self._authurl,
+            headers=self._postheaders,
+            data=json.dumps(self._authpayload),
+            verify=False,
+        )
+        if res.status_code != 200:
+            logger.warning("Request for authentication returned HTTP "
+                           f"{res.status_code}: {res.reason}")
+            if res.status_code < 200 or res.status_code > 299:
+                raise Exception("Could not retrieve access token.")
+        retjson = res.json()
+        return retjson["access_token"]
+
+    def _put_api(self, resource, payload):
+        return requests.put(
+            url=f"{self._baseurl}api/{resource}",
+            headers=self._postheaders,
+            data=json.dumps(payload),
+            verify=False,
+        )
+
+    def _get_api(self, resource, filter=None):
+        url = f"{self._baseurl}api/{resource}"
+        if filter:
+            url += f"?filter={urllib.parse.quote(json.dumps(filter))}"
+
+        return requests.get(
+            url=url,
+            headers=self._getheaders,
+            verify=False,
+        )
+
+    def get_info_for_mac_address(self, mac, filter={}):
+        if mac not in self._macinfo:
+            ret = {}
+            rmacfilter = {"mac_address": mac}
+            hmacfilter = {"mac": hyphenate_mac(mac)}
+            data = {}
+
+            data["endpointinfo"] = ("endpoint", rmacfilter)
+            data["deviceinfo"] = ("device", hmacfilter)
+            data["guestinfo"] = ("guest", hmacfilter)
+            data["sessioninfo"] = ("session", rmacfilter)
+            data["insightinfo"] = (f"insight/endpoint/mac/{mac}", None)
+
+            for name, (resource, filter) in data.items():
+                info = self._get_api(resource, filter=filter)
+                if info.status_code == 200:
+                    if name == "insightinfo":
+                        ret[name] = [info.json()]
+                    else:
+                        ret[name] = info.json()["_embedded"]["items"]
+                else:
+                    logger.warning(
+                        f"{name}: HTTP {info.status_code} - {info.reason}")
+
+            self._macinfo[mac] = ret
+        return self._macinfo[mac]
+
+    def get_mac_id(self, mac):
+        mac_info = self._get_api(f"endpoint/mac-address/{mac}").json()
+        try:
+            return mac_info["id"]
+        except KeyError:
+            raise Exception(f"MAC address {mac} not found on server")
+
+    def set_mac_address(
+            self, mac_id, mac, status, description=None, attributes=None):
+        data = {
+            "id": mac_id,
+            "mac_address": mac,
+            "status": status
+        }
+        if description is not None:
+            data["description"] = description
+
+        if attributes is not None:
+            data["attributes"] = attributes
+        return self._put_api(f"endpoint/{mac_id}", data)
+
+    def enable_mac_address(self, mac, mac_id):
+        return self.set_mac_address(mac_id, mac, status="Known")
+
+    def disable_mac_address(self, mac, mac_id, disabled_by, reason):
+        return self.set_mac_address(mac_id, mac, status="Disabled",
+                                    attributes={
+                                      "Disabled By": disabled_by,
+                                      "Disabled Reason": reason
+                                    })
