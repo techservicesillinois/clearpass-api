c314527cca510761407a343a696ef541db947e91
David D. Riddle <ddriddle@users.noreply.github.com>
2024-06-27 15:43:15 -0500

    Add setup.py (#9)

---
+++ src/clearpass/exceptions.py
@@ -0,0 +1,7 @@
+class ClearPassAPI(Exception):
+    pass
+
+class TokenError(ClearPassAPI):
+    def __init__(self) -> None:
+        mesg = "Could not retrieve access token."
+        super().__init__(mesg)
