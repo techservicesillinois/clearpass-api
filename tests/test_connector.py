729916fa7a3a82103b9e72515fa8978641051f85
Edward Delaporte <edward@delaporte.us>
2024-08-01 21:06:37 +0000

    Add tests for 404 and TODO

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -14,8 +14,24 @@ def test_disable_mac(cassette, clearpass_client):
     assert result
 
 
+def test_404_disable_mac(cassette, clearpass_client):
+    '''Test disable on a MAC that is not found.'''
+    result = clearpass_client.disable_mac_address(**TEST_DATA)
+    # TODO: Update to expect 404 error
+    assert result
+
+
 def test_enable_mac(cassette, clearpass_client):
     result = clearpass_client.enable_mac_address(
         mac=TEST_DATA['mac'],
         mac_id=TEST_DATA['mac_id'])
+    assert result
+
+
+def test_404_enable_mac(cassette, clearpass_client):
+    '''Test enable on a MAC that is not found.'''
+    result = clearpass_client.enable_mac_address(
+        mac=TEST_DATA['mac'],
+        mac_id=TEST_DATA['mac_id'])
+    # TODO: Update to expect 404 error
     assert result
