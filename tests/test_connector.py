aed1570bf2f2ee2ee7ce972b5bc3915e93e136c1
Tyler Turner <tzturner@illinois.edu>
2024-08-05 15:48:30 +0000

    WIP failing test

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1,4 +1,6 @@
 from conftest import TEST_DATA
+import pytest
+# from Exception import ConnectionRefusedError
 
 
 def test_connectivity(cassette, clearpass_client):
@@ -17,9 +19,8 @@ def test_disable_mac(cassette, clearpass_client):
 
 def test_404_disable_mac(cassette, clearpass_client):
     '''Test disable on a MAC that is not found.'''
-    result = clearpass_client.disable_mac_address(**TEST_DATA)
-    # TODO: Update to expect 404 error
-    assert result
+    with pytest.raises(ConnectionRefusedError):
+        clearpass_client.disable_mac_address(**TEST_DATA)
 
 
 def test_enable_mac(cassette, clearpass_client):
@@ -31,8 +32,7 @@ def test_enable_mac(cassette, clearpass_client):
 
 def test_404_enable_mac(cassette, clearpass_client):
     '''Test enable on a MAC that is not found.'''
-    result = clearpass_client.enable_mac_address(
-        mac=TEST_DATA['mac'],
-        mac_id=TEST_DATA['mac_id'])
-    # TODO: Update to expect 404 error
-    assert result
+    with pytest.raises(ConnectionRefusedError):
+        clearpass_client.enable_mac_address(
+            mac=TEST_DATA['mac'],
+            mac_id=TEST_DATA['mac_id'])
