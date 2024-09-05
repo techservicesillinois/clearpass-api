0239dd8f765ce2224325fa085d8a0bf30f3ab0b6
mpitcel <3997366+mpitcel@users.noreply.github.com>
2024-09-05 14:08:13 -0500

    Force Default Python behavior in `__init__.py` (#20)
    Co-authored-by: David Riddle <ddriddle@illinois.edu>
    Co-authored-by: Edward Delaporte <delaport@illinois.edu>
    Co-authored-by: Michelle Pitcel <mpitcel2@illinois.edu>

--- src/clearpass/__init__.py
+++ src/clearpass/__init__.py
@@ -1,7 +1,11 @@
+# DEBUG: Can the contents of this file be removed?
+# SOAR may be changing the default behavior.
+# The lines below simply perform what Python normally does.
+
 __all__ = [
     "client",
     "exceptions"
 ]
 
-# import clearpass.client
-# import clearpass.exceptions
+import clearpass.client    # noqa
+import clearpass.exceptions    # noqa
