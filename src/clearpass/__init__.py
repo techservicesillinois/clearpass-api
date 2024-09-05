1d7b9909f78dd521815de00086254ac55334fb40
mpitcel <3997366+mpitcel@users.noreply.github.com>
2024-09-05 14:01:35 -0500

    Update __init__.py
    Add a docstring for future reference

--- src/clearpass/__init__.py
+++ src/clearpass/__init__.py
@@ -1,3 +1,7 @@
+# DEBUG: Can the contents of this file be removed?
+# SOAR may be changing the default behavior.
+# The lines below simply perform what Python normally does.
+
 __all__ = [
     "client",
     "exceptions"
